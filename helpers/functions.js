1ee7a17e027c8c357af49151560fa02462c201ad
jhadev <joshappeldev@gmail.com>
2020-12-24 11:36:31 -0500

    cleanup index.js - put functions into helper file

--- helpers/functions.js
+++ helpers/functions.js
@@ -62,4 +62,74 @@ function runCMD(command) {
   });
 }
 
-module.exports = { gatherMetrics, convertURL, runCMD };
+/**
+ * @param {object} page object
+ * @param {string} page.url - url for the page
+ * @param {string} page.filename - filename for lh report
+ */
+
+async function runLighthouse(page) {
+  page.createTimestamp();
+  const { url, filename } = page;
+  currentFile = `${filename}.report`;
+  const path = `--output-path ${filename}.json`;
+  const command = `lighthouse ${url} --output json --output html ${path} --only-categories=performance --chrome-flags="--headless --disable-dev-shm-usage" --view`;
+  console.log(`
+  
+  ==============================
+  LIGHTHOUSE STARTED
+  ==============================
+
+  `);
+
+  // TODO: add why perf matters here
+
+  // run lh cmd
+  await runCMD(command);
+
+  console.log(`
+  
+  ==============================
+  LIGHTHOUSE FINISHED
+  ==============================
+  
+  `);
+
+  const metrics = await gatherMetrics(`${currentFile}.json`);
+
+  const table = Object.entries(metrics).map(([key, value]) => {
+    return [key, value.displayValue];
+  });
+  // print table
+  console.table(table);
+  // attach stats to page object
+  page.saveStats(Object.fromEntries(table));
+  console.log(page);
+  // TODO: print to sheets.
+}
+
+async function runCLS(page) {
+  const { url, filename } = page;
+  const command = `layout-shift-gif --url ${url} --device mobile --output ${filename}.gif`;
+  console.log(`
+  
+  ==============================
+  CLS STARTED
+  ==============================
+
+  `);
+
+  // TODO: add layout shift info here
+
+  await runCMD(command);
+
+  console.log(`
+  
+  ==============================
+  CLS FINISHED
+  ==============================
+
+  `);
+}
+
+module.exports = { gatherMetrics, convertURL, runCMD, runLighthouse, runCLS };
--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const inquirer = require("inquirer");
 const open = require("open");
 require("dotenv").config();
 const Page = require("./helpers/Page");
-const { gatherMetrics, runCMD } = require("./helpers/functions");
+const {
+  gatherMetrics,
+  runCMD,
+  runLighthouse,
+  runCLS,
+} = require("./helpers/functions");
 const { inputPrompt, clsPrompt } = require("./helpers/prompts");
 const { fetchSheet } = require("./sheets");
 const pageList = require("./helpers/page-list");
@@ -53,76 +58,6 @@ async function askForCLS(page) {
   }
 
   return;
-}
-
-/**
- * @param {object} page object
- * @param {string} page.url - url for the page
- * @param {string} page.filename - filename for lh report
- */
-
-async function runLighthouse(page) {
-  page.createTimestamp();
-  const { url, filename } = page;
-  currentFile = `${filename}.report`;
-  const path = `--output-path ${filename}.json`;
-  const command = `lighthouse ${url} --output json --output html ${path} --only-categories=performance --chrome-flags="--headless --disable-dev-shm-usage" --view`;
-  console.log(`
-  
-  ==============================
-  LIGHTHOUSE STARTED
-  ==============================
-
-  `);
-
-  // TODO: add why perf matters here
-
-  // run lh cmd
-  await runCMD(command);
-
-  console.log(`
-  
-  ==============================
-  LIGHTHOUSE FINISHED
-  ==============================
-  
-  `);
-
-  const metrics = await gatherMetrics(`${currentFile}.json`);
-
-  const table = Object.entries(metrics).map(([key, value]) => {
-    return [key, value.displayValue];
-  });
-  // print table
-  console.table(table);
-  // attach stats to page object
-  page.saveStats(Object.fromEntries(table));
-  console.log(page);
-  // TODO: print to sheets.
-}
-
-async function runCLS(page) {
-  const { url, filename } = page;
-  const command = `layout-shift-gif --url ${url} --device mobile --output ${filename}.gif`;
-  console.log(`
-  
-  ==============================
-  CLS STARTED
-  ==============================
-
-  `);
-
-  // TODO: add layout shift info here
-
-  await runCMD(command);
-
-  console.log(`
-  
-  ==============================
-  CLS FINISHED
-  ==============================
-
-  `);
 }
 
 (async () => {
